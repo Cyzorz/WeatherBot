7f9bb74fefe07c7959902d1d437ed548e2bcbd06
Cyzorz <root@vps.server.local>
2021-01-22 22:08:43 -0800

    More cleanup

--- config.py
+++ config.py
@@ -1,3 +1,1 @@
 BOT_TOKEN = "your_token_here"
-
-WEBHOOK_URL = "your_webhook_here"
--- forecast.py
+++ forecast.py
@@ -1,91 +1,88 @@
-#file dependencies
-import weatherHandler as forecast
+# File dependencies
 import config as cfg
 import colorHandler as color
-#Command Dependencies
+# Command Dependencies
 import discord
 from discord.ext import commands
-#Data Dependencies
+# Data Dependencies
 import json
 import requests
-#Image dependencies
+# Image dependencies
 from skimage import io, img_as_float
 import urllib.request
 import io as sysio
 
 
-#command prefix for weather
+# Command Prefix
 client = commands.Bot(command_prefix="c!")
 
-#presence, console message 
+# Initialization
 @client.event
 async def on_ready():
-    print(client.user.name + " is ready, ID: " + str(client.user.id))
+    print("Ready!")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"The Forecast!"))
 
-#weather command, handler(s) not attatched (refer to next commit)
+# Weather command
 @client.command()
 async def weather(message, *args):
-    await message.channel.trigger_typing()
-#URL json data to load on embed
+    # Check for errors
+    if len(args) < 1:
+        await message.channel.send(embed = discord.Embed(title="Error ❌", description = "Please enter a location!", color = 0xd63f3f))
+        return True
+    # Get API request key and query
     params = {
         'access_key': 'f94750a0bd28417e135bb2c85b4ae93e',
         'query': " ".join(args)
     }
-    i = 0
-    if i == 0:
-    #Embed data from JSON data URL
-        r = requests.get("http://api.weatherstack.com/current", params)
-        response = r.json()
-        current = response['current']
-        request = response['request']
-        location = response['location']
-        data = {
-            "content": "WeatherBot",
-            "embeds": [
-                {
-                    "title": "Weather",
-                    "description": f"Weather for {request['query']}",
-                    "thumbnail": {"url": current['weather_icons']},
-                    "color": 808080,
-                    "fields": [
-                        {
-                            "name": "Celsius (°C)",
-                            "value": f"{current['temperature']}° C",
-                        },
-                        {
-                           "name": "Farenheit (°F)",
-                           "value": f"{round((current['temperature'] * 9/5) + 32)}° F"
-                        },
-                        {
-                            "name": "Humidity",
-                            "value": f"{current['humidity']}"
-                        }
-                    ],
-                    "footer": {"text":f"Last Updated: {current['observation_time']} ({location['name']})"}
-                }
-            ]
-        }
-
-    # Actual embed text from JSON data
-        embed = discord.Embed(title = data['embeds'][0]['title'], description = data['embeds'][0]['description'], color = data['embeds'][0]['color'])
-        embed.set_thumbnail(url = f"{current['weather_icons'][0]}")
-        for fields in data["embeds"]:
-            embed.add_field(name = data['embeds'][0]['fields'][0]['name'], value = data['embeds'][0]['fields'][0]['value'], inline=False)
-            return embed
-            
-        embed.set_footer(text = data['embeds'][0]['footer']['text'])
-
-        await message.channel.send(embed=embed)
-
-@client.command()
-async def url(message):
-    embed = discord.Embed(
-        title = "Hi",
-        color = 0xffffff,
-        description = "Hello"
-    )
-    fields = {
+    r = requests.get("http://api.weatherstack.com/current", params)
+    response = r.json()
+    current = response['current']
+    request = response['request']
+    location = response['location']
+#Get Info via Handler
+    data = {
+        "embeds": [
+            {
+                "title": "Weather",
+                "description": f"Forecast for {request['query']}",
+                "color": 808080,
+                "fields": [
+                    {
+                        "name": "Celsius (°C)",
+                        "value": f"{current['temperature']}° C",
+                    },
+                    {
+                        "name": "Farenheit (°F)",
+                        "value": f"{round((current['temperature'] * 9/5) + 32)}° F"
+                    },
+                    {
+                        "name": "Humidity",
+                        "value": f"{current['humidity']}"
+                    },
+                    {
+                        "name": "Observation Time",
+                        "value": f"{current['observation_time']}"
+                    },
+                    {
+                        "name": "Weather Description",
+                        "value": f"{current['weather_descriptions'][0]}"
+                    }
+                ],
+                "footer": {"text":f"Last Updated: {current['observation_time']} ({location['name']})"}
+            }
+        ]
     }
+
+# Actual embed text from JSON data
+    embed = discord.Embed(title = data['embeds'][0]['title'], description = data['embeds'][0]['description'], color = data['embeds'][0]['color'])
+    embed.set_thumbnail(url = f"{current['weather_icons'][0]}")
+    embed.add_field(name = data['embeds'][0]['fields'][0]['name'], value = data['embeds'][0]['fields'][0]['value'], inline = True)
+    embed.add_field(name = data['embeds'][0]['fields'][1]['name'], value = data['embeds'][0]['fields'][1]['value'], inline= True)
+    embed.add_field(name = data['embeds'][0]['fields'][2]['name'], value = data['embeds'][0]['fields'][2]['value'], inline= False)
+    embed.add_field(name = data['embeds'][0]['fields'][3]['name'], value = data['embeds'][0]['fields'][3]['value'], inline = True)
+    embed.add_field(name = data['embeds'][0]['fields'][4]['name'], value = data['embeds'][0]['fields'][4]['value'], inline = False)
+    embed.set_footer(text = data['embeds'][0]['footer']['text'])
     await message.channel.send(embed=embed)
-#run bot
+
+# Run bot
 client.run(cfg.BOT_TOKEN)
